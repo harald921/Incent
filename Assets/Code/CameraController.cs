334111cd165c094a1c2548fddb1b402091268ff1
Harald <harald921@hotmail.com>
2018-05-11 05:43:19 +0200

    Implement basic camera controls

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float _baseSpeed = 10.0f;
+    [SerializeField] float _shiftMultiplier = 1.3f;
+
+	void Update()
+    {
+        Vector3 movement = new Vector3()
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            z = Input.GetAxisRaw("Vertical")
+        };
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            movement *= _shiftMultiplier;
+
+        transform.position += movement * _baseSpeed * Time.deltaTime;
+    }
+}
